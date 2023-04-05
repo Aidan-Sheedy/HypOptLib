116df6dc897e0baddc38512987597229662aa551
Aidan Sheedy <Aidan.P.Sheedy@gmail.com>
2023-04-04 22:39:35 -0400

    Missed file on previous commit

---
+++ include/LagrangeMultiplier.h
@@ -0,0 +1,27 @@
+
+#pragma once
+
+#include "TopOpt.h"
+#include "Filter.h"
+
+#include <petsc.h>
+
+class LagrangeMultiplier
+{
+    public:
+        LagrangeMultiplier(){}
+
+        LagrangeMultiplier(Filter *filter, TopOpt *opt)
+        {
+            this->filter    = filter;
+            this->opt       = opt;
+        }
+
+        PetscScalar computeLagrangeMultiplier(Vec positions, Vec C, PetscInt numParticles, PetscScalar *returnValue);
+
+    private:
+        Filter *filter;
+
+        TopOpt *opt;
+
+};
